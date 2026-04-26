ce71c8d83462fc3225a5d719cf0611f3aff2dd5b
w.hidzy <oficialwahid9@gmail.com>
2026-04-26 15:07:53 +0700

    Update add-testi.js

--- api/add-testi.js
+++ api/add-testi.js
@@ -5,10 +5,16 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).send("Method Not Allowed");
 
-    const { url } = req.body;
+    const { url, layanan, jumlah, price } = req.body;
+    
     const { data, error } = await supabase
         .from('testimonials')
-        .insert([{ image_url: url }]);
+        .insert([{ 
+            image_url: url, 
+            layanan: layanan, 
+            jumlah: jumlah, 
+            price: price 
+        }]);
 
     if (error) return res.status(500).json(error);
     return res.status(200).json({ success: true });
